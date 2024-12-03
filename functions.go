986507b86df29bfc6da51fd7c268bf7f9531e308
Chris King <cking5836@gmail.com>
2024-12-03 17:29:23 -0500

    adds function structure

--- functions.go
+++ functions.go
@@ -1,5 +1,28 @@
 package main
 
-func compress(data []byte) ([]byte, error) {
+import "log"
+
+func compress(data []byte, encodingType string) ([]byte, error) {
+	if encodingType == "RLE" {
+		err := runLength(&data)
+		if err != nil {
+			log.Fatal("There was an error: ", err)
+		}
+
+		if encodingType == "huffman" {
+			err := huffman(&data)
+			if err != nil {
+				log.Fatal("There was an error: ", err)
+			}
+		}
+	}
+	return data, nil
+}
+
+func runLength(data *[]byte) error {
+	return data, nil
+}
+
+func huffman(data *[]byte) error {
 	return data, nil
 }
